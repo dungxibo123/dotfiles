fe2ea5611da925dbb3b4cfbab759b04572a45b3b
Vo Tien Dung <41417288+dungxibo123@users.noreply.github.com>
2022-01-03 05:23:32 +0700

    Create basic-setup.sh

---
+++ basic-setup.sh
@@ -0,0 +1,21 @@
+# Use as root
+su
+#Type password in
+
+# install the git and the fakeroot enviroment
+pacman -S --needed git base-devel
+
+# Clone the yay repository and install yay packages manager
+# git clone https://aur.archlinux.org/yay-bin.git
+cd /opt
+git clone https://aur.archlinux.org/yay-git.git
+chown -R <YOUR_USER_NAME_HERE>:<YOUR_USER_NAME_HERE> ./yay-git
+
+cd yay-git
+makepkg -si
+
+# Update  (synchronize with pacman ??) the mirror list for yay
+yay -Sy
+yay -Syy
+
+yay -S $(awk '{print $1}'  usePackages.txt)
