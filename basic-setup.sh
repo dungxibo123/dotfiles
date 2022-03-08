58d0d27efa76f58e83c666d7d5ccd737f2de4876
dungxibo123 <votiendung0805@gmail.com>
2022-03-08 10:09:51 +0700

    add the basic-setup.sh file

--- basic-setup.sh
+++ basic-setup.sh
@@ -5,8 +5,8 @@ su
 # install the git and the fakeroot enviroment
 pacman -S --needed git base-devel
 
-# Clone the yay repository and install yay packages manager
 # git clone https://aur.archlinux.org/yay-bin.git
+# Clone the yay repository and install yay packages manager
 cd /opt
 git clone https://aur.archlinux.org/yay-git.git
 chown -R <YOUR_USER_NAME_HERE>:<YOUR_USER_NAME_HERE> ./yay-git
@@ -19,3 +19,11 @@ yay -Sy
 yay -Syy
 
 yay -S $(awk '{print $1}'  usePackages.txt)
+
+cd ~
+git clone https://github.com/dungxibo123/dotfiles
+
+
+cp -r dotfiles/*/*/*/* ./
+
+
