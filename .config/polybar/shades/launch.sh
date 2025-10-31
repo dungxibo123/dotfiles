4aa77a30fc23a2b149234627eed23011825595fe
dungxibo123 <votiendung0805@gmail.com>
2025-10-30 23:21:21 -0400

    test

--- .config/polybar/shades/launch.sh
+++ .config/polybar/shades/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # Add this script to your wm startup file.
 
@@ -11,4 +11,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+#
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+done
